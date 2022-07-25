3b9399038e049a67aac9ef120de8af9d9867e01d
tristan472 <65858998+tristan472@users.noreply.github.com>
2022-07-25 22:07:49 +0200

    spring-demo-aop

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,7 +12,8 @@ public class MyDemoLoggingAspect {
 	
 	// let's start with an @before advice
 	
-	@Before("execution(public void addAccount())")
+	//@Before("execution(public void add*())")
+	@Before("execution(* add*())")
 	public void beforeAddAccountAdvice() {
 		
 		System.out.println("\n====>>>Executing @Before advice on method");
